547a61d01b9e88e5fe10ca3420597f35a1476a1f
Garulf <535299+Garulf@users.noreply.github.com>
2022-12-29 14:12:02 -0500

    Use Flox/Flows string matching score

--- plugin/main.py
+++ plugin/main.py
@@ -1,4 +1,5 @@
 from flox import Flox, ICON_HISTORY, ICON_BROWSER
+from flox.string_matcher import string_matcher
 
 from browsers import BROWSERS
 
@@ -17,6 +18,7 @@ class BrowserHistory(Flox):
         super().__init__()
         self.default_browser = self.settings.get('default_browser', DEFAULT_BROWSER).lower()
         self.browser = BROWSERS[self.default_browser]
+
     def _query(self, query):
         try:
             self.query(query)
@@ -25,23 +27,33 @@ class BrowserHistory(Flox):
                 title='History not found!',
                 subtitle='Check your logs for more information.',
             )
+        except Exception as e:
+            self.add_item(
+                title='Something went wrong!',
+                subtitle=f'Check your logs for more information: {e}',
+            )
+            self.logger.exception(e)
         finally:
             return self._results
 
     def query(self, query):
         history = self.browser.get_history(limit=10000)
         for idx, item in enumerate(history):
-            if query.lower() in item.title.lower() or query.lower() in item.url.lower():
-                subtitle = f"{idx}. {item.url}"
-                self.add_item(
-                    title=item.title,
-                    subtitle=subtitle,
-                    icon=ICON_HISTORY,
-                    glyph=HISTORY_GLYPH,
-                    method=self.browser_open,
-                    parameters=[item.url],
-                    context=[item.title, item.url]
-                )
+            title_match = string_matcher(query, item.title)
+            title_score = title_match.score if title_match else 0
+            url_match = string_matcher(query, item.url)
+            url_score = url_match.score if url_match else 0
+            subtitle = f"{idx}. {item.url}"
+            self.add_item(
+                title=item.title,
+                subtitle=subtitle,
+                icon=ICON_HISTORY,
+                glyph=HISTORY_GLYPH,
+                method=self.browser_open,
+                parameters=[item.url],
+                context=[item.title, item.url],
+                score=int(max(title_score, url_score)),
+            )
 
     def context_menu(self, data):
         self.add_item(
